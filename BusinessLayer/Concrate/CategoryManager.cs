41e1bf9609de2c11eeac6b0761446988f1e85754
Ali Kaan Yayla <naklovcuas@gmail.com>
2023-04-19 16:01:03 +0300

    Add project files.

---
+++ BusinessLayer/Concrate/CategoryManager.cs
@@ -0,0 +1,47 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrate
+{
+    public class CategoryManager : ICategoryService
+    {
+        EfCategoryRepository _efCategoryRepository;
+
+        public CategoryManager(EfCategoryRepository efCategoryRepository)
+        {
+            _efCategoryRepository = efCategoryRepository;
+        }
+
+        public void Add(Category category)
+        {
+            _efCategoryRepository.Insert(category);
+        }
+
+        public void Delete(Category category)
+        {
+            _efCategoryRepository.Delete(category);
+        }
+
+        public Category GetByID(int id)
+        {
+            return _efCategoryRepository.GetByID(id);
+        }
+
+        public List<Category> GetList()
+        {
+            return _efCategoryRepository.ListAll();
+        }
+
+        public void Update(Category category)
+        {
+            _efCategoryRepository.Update(category);
+        }
+    }
+}
---
+++ EntityLayer/Concrate/Comment.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrate
+{
+    public class Comment
+    {
+        [Key]
+        public int CommentID { get; set; }
+
+        public string CommentUserName { get; set; }
+
+        public string CommentTitle { get; set; }
+
+        public string CommentContent { get; set; }
+
+        public DateTime MyProperty { get; set; }
+
+        public bool CommentStatus { get; set; }
+
+        public int BlogID { get; set; }
+
+        public Blog Blog { get; set; }
+    }
+}
